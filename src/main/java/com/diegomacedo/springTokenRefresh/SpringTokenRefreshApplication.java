54614299772157a3d8cfb7ba120911519468a26c
Diego Notebook <diegomacedo_cp@hotmail.com>
2021-08-24 20:39:16 -0300

    First commit

---
+++ src/main/java/com/diegomacedo/springTokenRefresh/SpringTokenRefreshApplication.java
@@ -0,0 +1,13 @@
+package com.diegomacedo.springTokenRefresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTokenRefreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTokenRefreshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegomacedo/springTokenRefresh/SpringTokenRefreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegomacedo.springTokenRefresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTokenRefreshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
