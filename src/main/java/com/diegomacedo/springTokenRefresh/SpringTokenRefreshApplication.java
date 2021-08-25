6679c6146fa4e11bc22cf671d43fa3cea061f117
DiegoMacedo <diegomacedo_cp@hotmail.com>
2021-08-25 15:14:44 -0300

    Configurando segurança do projeto spring

--- src/main/java/com/diegomacedo/springTokenRefresh/SpringTokenRefreshApplication.java
+++ src/main/java/com/diegomacedo/springTokenRefresh/SpringTokenRefreshApplication.java
@@ -2,12 +2,21 @@ package com.diegomacedo.springTokenRefresh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringTokenRefreshApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTokenRefreshApplication.class, args);
+	}
+	
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 }
--- src/main/java/com/diegomacedo/springTokenRefresh/service/UserServiceImpl.java
+++ src/main/java/com/diegomacedo/springTokenRefresh/service/UserServiceImpl.java
@@ -1,9 +1,15 @@
 package com.diegomacedo.springTokenRefresh.service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.diegomacedo.springTokenRefresh.domain.Role;
@@ -14,13 +20,34 @@ import com.diegomacedo.springTokenRefresh.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements UserService{
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService, UserDetailsService {
 
 	private final UserRepository userRepo;
 	private final RoleRepository roleRepo;
-	 
-	
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepo.findByUsername(username);
+		if (user == null) {
+			log.error("Usuário não encontrado no banco");
+			throw new UsernameNotFoundException("Usuário não encontrado no banco");
+		} else {
+			log.info("Usuário encontrado no banco {}", username);
+		}
+
+		Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		user.getRoles().forEach(role -> {
+			authorities.add(new SimpleGrantedAuthority(role.getName()));
+		});
+
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+				authorities);
+	}
+
 	@Override
 	public User saveUser(User user) {
 		log.info("Salvando novo usuario {} no banco de dados", user.getName());
@@ -29,25 +56,25 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public Role saveRole(Role role) {
-		log.info("Salvando novo role {} no banco de dados",role.getName());
+		log.info("Salvando novo role {} no banco de dados", role.getName());
 		return roleRepo.save(role);
 	}
 
 	@Override
 	public void addRoleToUser(String username, String roleName) {
-		
-		log.info("Adicionando role {} no usuario {}",username,roleName);
-		
+
+		log.info("Adicionando role {} no usuario {}", username, roleName);
+
 		User user = userRepo.findByUsername(username);
 		Role role = roleRepo.findByName(roleName);
-		
+
 		user.getRoles().add(role);
-		
+
 	}
 
 	@Override
 	public User getUser(String username) {
-		log.info("Pesquisando usuario {}",username);
+		log.info("Pesquisando usuario {}", username);
 		return userRepo.findByUsername(username);
 	}
 
