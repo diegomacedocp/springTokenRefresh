dad3aac6095970766917f9f94a5433837ceb2e97
DiegoMacedo <diegomacedo_cp@hotmail.com>
2022-07-10 10:17:19 -0300

    Colocando o acess token para 1min na hora de efetuar o refresh token

--- src/main/java/com/diegomacedo/springTokenRefresh/api/UserResource.java
+++ src/main/java/com/diegomacedo/springTokenRefresh/api/UserResource.java
@@ -77,7 +77,7 @@ public class UserResource {
 
 				String acess_token = JWT.create()
 						.withSubject(user.getUsername())
-						.withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+						.withExpiresAt(new Date(System.currentTimeMillis() + 1 * 60 * 1000))
 						.withIssuer(request.getRequestURI().toString())
 						.withClaim("roles", user.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
 						.sign(algorithm);
